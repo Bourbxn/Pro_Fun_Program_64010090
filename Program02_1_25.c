631bed54bef6d3c77ec742d1861b9b7eedcfd7ab
Bourbxn <jakkapat97@gmail.com>
2021-08-13 00:26:10 +0700

    MSI

---
+++ Program02_1_25.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int num;
+  scanf("%d",&num);
+  for(int i=1;i<=num;i++){
+    for(int j=1;j<=num-i;j++){
+      printf(" ");
+    }
+    for(int j=1;j<=2*i-1;j++){
+      printf("*");
+    }
+    if(i!=num)printf("\n");
+  }
+}
