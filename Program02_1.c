b2fa1708fa9ec67ba2f0a38290cd7636cdca1b65
Bourbxn <jakkapat97@gmail.com>
2021-08-14 20:57:45 +0700

    PC

---
+++ Program02_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+  int sequence,prime_seq=1,start_num=2,prime_num;
+  printf("Sequence of Prime number : ");
+  scanf("%d",&sequence);
+  while(prime_seq<sequence){
+    start_num++;
+    int fac=2,fac_check=0;
+    while(fac<=start_num){
+      if(start_num%fac==0){
+        fac_check+=1;
+        fac++;
+      }
+      else{fac++;}
+    }
+    if(fac_check==1)prime_seq++;
+    if(prime_seq==sequence)prime_num=start_num;
+  }
+  printf("This Prime number >> %d", start_num);
+}
