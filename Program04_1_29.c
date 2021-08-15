354fe8150bb21994f5f6cd42ce5c7a167ffa21aa
Bourbxn <jakkapat97@gmail.com>
2021-08-16 00:41:42 +0700

    PC

--- Program04_1_29.c
+++ Program04_1_29.c
@@ -1,8 +1,18 @@
 #include "stdio.h"
 int main(){
   float s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,avg;
+  printf("Enter Height of 10 students : ");
   scanf("%f %f %f %f %f %f %f %f %f %f", &s1,&s2,&s3,&s4,&s5,&s6,&s7,&s8,&s9,&s10);
   avg=(s1+s2+s3+s4+s5+s6+s7+s8+s9+s10)/10;
-  printf("%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f\n",s1,s2,s3,s4,s5,s6,s7,s8,s9,s10);
-  printf("%.2f", avg);
+  printf("Student 1 : %.2f\n", s1);
+  printf("Student 2 : %.2f\n", s2);
+  printf("Student 3 : %.2f\n", s3);
+  printf("Student 4 : %.2f\n", s4);
+  printf("Student 5 : %.2f\n", s5);
+  printf("Student 6 : %.2f\n", s6);
+  printf("Student 7 : %.2f\n", s7);
+  printf("Student 8 : %.2f\n", s8);
+  printf("Student 9 : %.2f\n", s9);
+  printf("Student 10 : %.2f\n", s10);
+  printf("Average Height of 10 students >> %.2f", avg);
 }
--- Program04_2_29.c
+++ Program04_2_29.c
@@ -1,13 +1,11 @@
 #include "stdio.h"
 int main(){
   float height[10],sum=0;
+  printf("Enter Height of 10 students : ");
   for(int i=0;i<10;i++){
     scanf("%f", &height[i]);
     sum+=height[i];
   }
-  for(int i=0;i<10;i++){
-    if(i==9)printf("%.2f\n", height[i]);
-    else{printf("%.2f ", height[i]);}
-  }
-  printf("%.2f", sum/10);
+  for(int i=0;i<10;i++)printf("Student %d : %.2f\n", i+1,height[i]);
+  printf("Average Height of 10 students >> %.2f", sum/10);
 }
