f76a520cbb14f650bd6ab73c454975d81f1aa4d9
Bourbxn <jakkapat97@gmail.com>
2021-08-15 23:55:27 +0700

    PC

--- Program10_2_12.c
+++ Program10_2_12.c
@@ -1,16 +1,13 @@
 #include "stdio.h"
 int main(){
-  int lst[3],min,max,sum=0;
+  int lst[3],max,sum=0;
   for(int i=0;i<3;i++){
     scanf("%d", &lst[i]);
     sum+=lst[i];
   }
   for(int i=0;i<3;i++){
-    if(i==0)min=lst[i];
-    if(lst[i]<=min)min=lst[i];
+    if(i==0)max=lst[i];
+    if(lst[i]>=max)max=lst[i];
   }
-  for(int i=0;i<3;i++){
-    if(lst[i]>=min)max=lst[i];
-  }
-  printf("%d",max);
+  printf("%d",sum-max);
 }
