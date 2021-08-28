43d10e6bdca6e4a0070994947d6f035652e6a8f8
Bourbxn <jakkapat97@gmail.com>
2021-08-29 00:38:02 +0700

    MSI

--- Program17_2_22.c
+++ Program17_2_22.c
@@ -3,7 +3,6 @@ int main(){
   int num,i=1,check=1;
   scanf("%d", &num);
   while(i<=num*num){
-    printf("%d\n", i);
     if(check==1)printf("*");
     else if(i%(num*2)==0)printf("");      //Remove Space
     else if(i%num==0)printf("");      //Remove Space
