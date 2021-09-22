9c73222ff7af9d7d35ae5cdb519160a24eae3f7b
Bourbxn <jakkapat97@gmail.com>
2021-09-22 14:46:31 +0700

    MSI

--- Program_week_07/Program_week_07_1.c
+++ Program_week_07/Program_week_07_1.c
@@ -2,7 +2,8 @@
 int main(){
   int num;
   scanf("%d",&num );
-  if(num%2!=0){
+  if(num<=0) printf("ERROR");
+  else if(num%2!=0){
     for(int i=1;i<=(num-1)/2;i++){      //PART TOP
       for(int j=1;j<=((num+1)/2)-1;j++) printf("* ");
       printf("*\n");
--- Program_week_07/Program_week_07_2.c
+++ Program_week_07/Program_week_07_2.c
@@ -2,15 +2,18 @@
 int main(){
   int num,i=1,check=1;
   scanf("%d", &num);
-  while(i<=num*num){
-    if(check==1)printf("*");
-    else if(i%(num*2)==0)printf("");      //Remove Space
-    else if(i%num==0)printf("");      //Remove Space
-    else printf(" ");
-    check = !check;
-    if(i%num==0 && i!=num*num)printf("\n");
-    if(num%2==0 && i%num==0)check = !check;
-    i++;
+  if(num<=0) printf("ERROR");
+  else{
+    while(i<=num*num){
+      if(check==1)printf("*");
+      else if(i%(num*2)==0)printf("");      //Remove Space
+      else if(i%num==0)printf("");      //Remove Space
+      else printf(" ");
+      check = !check;
+      if(i%num==0 && i!=num*num)printf("\n");
+      if(num%2==0 && i%num==0)check = !check;
+      i++;
+    }
   }
   return 0;
 }
